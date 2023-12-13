a02fae37d865c959997552e795917ceb6baaa62f
rgraue <rgraue@outlook.com>
2023-12-12 17:14:28 -0800

    make purty

--- src/java/PrimeSieve.java
+++ src/java/PrimeSieve.java
@@ -8,34 +8,37 @@ public class PrimeSieve {
     public static void main (String[] args) {
         byte[] nums = new byte[CEILING];
 
+        // benchmark start
         Instant start = Instant.now();
 
+        // do the thing!
         findPrimes(nums);
 
+        // benchmark stop
         Instant stop = Instant.now();
         System.out.println(Summary(nums, start, stop));
-
-
-       
-    
     }
 
+    // controller to find primes up to CEILING
     private static void findPrimes(byte[] nums) {
         nums[0] = 1;
         nums[1] = 1;
         for (int i = 2; i < CEILING / 2; i++) {
-            if (nums[i] == 0) {
-                markMultiples(nums, i);
+            if (nums[i] == 0) { // if not marked as non-prime, it must be prime
+                markMultiples(nums, i); // then mark all multiples of it
             }
         }
     }
 
+    // Mark all multiples of instance up to CEILING
+    // none of those will not be primes
     private static void markMultiples(byte[] nums, int n) {
         for (int i = n * 2; i < CEILING; i+= n) {
             nums[i] = 1;
         }
     }
 
+    // fancy DEBUG logging
     private static String arrToString(byte[] nums) {
         StringBuilder s = new StringBuilder();
         for (int i = 0; i < CEILING; i++) {
@@ -46,6 +49,8 @@ public class PrimeSieve {
         return s.toString();
     }
 
+    // provide summary of pass
+    // dont care ab speed here. happen post testing.
     private static String Summary(byte[] nums, Instant start, Instant stop) {
         int total = 0;
         var diff = stop.toEpochMilli() - start.toEpochMilli();
